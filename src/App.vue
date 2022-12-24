274895fe9a075d1d85a2426208374134f5c1fe51
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-23 23:40:33 -0300

    custom event

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,18 @@ export default {
   data(){
     return{
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null
   }
 },
   methods: {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
     }
   }
 }
@@ -22,7 +27,9 @@ export default {
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p>Reaction time {{score}} ms</p>
+
 </template>
 
 <style>

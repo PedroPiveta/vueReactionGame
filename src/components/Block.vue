2ebabac76958aa48c803a168aca12b6077b4d9ec
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-23 23:35:25 -0300

    timer creation

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
         Click me
     </div>
 </template>
@@ -9,15 +9,29 @@ export default{
     props: ['delay'],
     data(){
         return{
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
         }
     },  
     mounted(){
         setTimeout(() => {
             this.showBlock = true
+            this.startTimer()
             console.log(this.delay)
         }, this.delay)    
     },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+        }
+    }
 }
 </script>
 
